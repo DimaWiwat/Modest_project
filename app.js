327de9b7c980be07f2269e1cd45d27cfcd1a9730
Dmitry <d.wygoniajlo@gmail.com>
2021-02-22 16:18:03 +0200

    added modal-menu

--- app.js
+++ app.js
@@ -12,3 +12,49 @@ mobileMenu.addEventListener("click", function() {
    }  
 
 })
+
+
+
+// *** модальное окно 
+
+let moreDetailsBtns = document.querySelectorAll(".details-btn");
+let modal = document.querySelector(".modal");
+let closeBtn = document.querySelector(".btn-close");
+
+function closeModal() {
+    modal.classList.remove("show");
+    modal.classList.add("hide")
+}
+
+function openModal() {
+    modal.classList.add("show");
+    modal.classList.remove("hide")
+}
+
+moreDetailsBtns.forEach((btn) => {
+    btn.addEventListener("click",function(e) {
+        e.preventDefault();
+        openModal()
+    })
+})
+
+closeBtn.addEventListener("click",closeModal)
+
+
+
+modal.addEventListener('click', function(e) {
+   if(e.target === modal) {
+       closeModal()
+   }
+})
+
+
+
+function showModalByScroll() {
+   if (window.pageYOffset > document.documentElement.scrollHeight/2) {
+       openModal();
+       window.removeEventListener("scroll", showModalByScroll)
+   }
+}
+
+window.addEventListener("scroll",showModalByScroll)
